f6cda32c7402aef6ff7b795f5a75785bafe4c75f
nsoltysiak <43788511+nsoltysiak@users.noreply.github.com>
2018-10-22 19:14:38 -0400

    Add files via upload

--- login.php
+++ login.php
@@ -1,91 +1,31 @@
-<?php
-
-    session_start();
-
-    $server = "localhost"; 
-    $userName = "mauricefuentes"; 
-    $pass = "E1i9s0Z5"; 
-    $db = "mauricefuentes";
-
-    //create connection
-    $con=mysqli_connect($server,$userName,$pass,$db); 
-    // Check connection
-    if (mysqli_connect_errno()) { 
-        echo "Failed to connect to MySQL: " . mysqli_connect_error(); 
-    }
-    
-    if (isset($_POST['login_btn'])) {
-        $email = mysql_real_escape_string($_POST['email']);
-        $password = mysql_real_escape_string($_POST['password']);
-        
-        $password = md5($password);
-        $db = "SELECT * FROM users WHERE email = '$email' AND password = '$password'";
-        $result = mysqli_query($con, $db);
-        
-        if (mysqli_num_rows($result) == 1) {
-            $_SESSION['message'] = "You are now loggeged in";
-            $_SESSION['email'] = $email;
-            header("location: home.php");
-        } else {
-            $_SESSION['message'] = "Email/password combination incorrect";
-        }
-    }
-
-?>
-
+<?php include('server.php') ?>
 <!DOCTYPE html>
 <html>
-
 <head>
-    <title>Login | OU Clubs</title>
-    <link rel="stylesheet" type="text/css" href="style.css">
+  <title>Registration system PHP and MySQL</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
 </head>
-
 <body>
-    <div data-role="header">
-            <h1 style="text-align: center;">
-                OU CLUBS
-            </h1>
-        </div>
-    
-    <div class="header">
-        <h1 style="text-align:center;">Login</h1>
-    </div>
-
-   
-    
-    <form method="post" action="login.php">
-        <table align="center" width="90%">
-            <tr>
-                <td>OU Email:</td>
-            </tr>
-            <tr>
-                <td><input type="email" name="email" class="textInput" pattern="[a-z0-9._%+-]+@oakland.edu$" placeholder="Enter a valid Oakland.edu email" required></td>
-            </tr>
-            <tr>
-                <td>Password:</td>
-                </tr>
-            <tr>
-                <td><input type="password" name="password" class="textInput" required></td>
-            </tr>
-            <tr>
-                <td><input type="submit" name="login_btn" value="login"></td>
-            </tr>
-            <tr>
-                
-            <td>Dont have an account?</td>
-            <tr>
-                <td>
-            <a href="register.php"><button>Create an Account</button></a>
-            </td>
-            </tr>
-            <tr>
-                <td>
-            <a href="index.html"><button>Go Back</button></a>
-            </td>
-            </tr>
-        </table>
-    </form>
+  <div class="header">
+  	<h2>Login</h2>
+  </div>
+	 
+  <form method="post" action="login.php">
+  	<?php include('errors.php'); ?>
+  	<div class="input-group">
+  		<label>Email</label>
+  		<input type="text" name="email" >
+  	</div>
+  	<div class="input-group">
+  		<label>Password</label>
+  		<input type="password" name="password">
+  	</div>
+  	<div class="input-group">
+  		<button type="submit" class="btn" name="login_user">Login</button>
+  	</div>
+  	<p>
+  		Not yet a member? <a href="register.php">Sign up</a>
+  	</p>
+  </form>
 </body>
-
 </html>
--- server.php
+++ server.php
@@ -1,80 +1,83 @@
 <?php
-
 session_start();
 
-    $server = "localhost"; 
-    $userName = "mauricefuentes"; 
-    $pass = "E1i9s0Z5"; 
-    $db = "mauricefuentes";
+// initializing variables
+$firstname = "";
+$lastname = "";
+$email    = "";
+$errors = array(); 
 
-    
+// connect to the database
+$db = mysqli_connect('localhost', 'nsoltysiak', 'chewytony', 'nsoltysiak');
 
-    //create connection
-    $con=mysqli_connect($server,$userName,$pass,$db); 
-    // Check connection
-    if (mysqli_connect_errno()) { 
-        echo "Failed to connect to MySQL: " . mysqli_connect_error(); 
+// REGISTER USER
+if (isset($_POST['reg_user'])) {
+  // receive all input values from the form
+  $firstname = mysqli_real_escape_string($db, $_POST['firstname']);
+  $lastname = mysqli_real_escape_string($db, $_POST['lastname']);
+  $email = mysqli_real_escape_string($db, $_POST['email']);
+  $password_1 = mysqli_real_escape_string($db, $_POST['password_1']);
+  $password_2 = mysqli_real_escape_string($db, $_POST['password_2']);
+
+  // form validation: ensure that the form is correctly filled ...
+  // by adding (array_push()) corresponding error unto $errors array
+  if (empty($firstname)) { array_push($errors, "First name is required"); }
+  if (empty($lastname)) { array_push($errors, "Last name is required"); } 
+  if (empty($email)) { array_push($errors, "Email is required"); }
+  if (empty($password_1)) { array_push($errors, "Password is required"); }
+  if ($password_1 != $password_2) {
+	array_push($errors, "The two passwords do not match");
+  }
+
+  // first check the database to make sure 
+  // a user does not already exist with the same email
+  $user_check_query = "SELECT * FROM users WHERE email='$email' LIMIT 1";
+  $result = mysqli_query($db, $user_check_query);
+  $user = mysqli_fetch_assoc($result);
+  
+  if ($user) { // if user exists
+    if ($user['email'] === $email) {
+      array_push($errors, "email already exists");
     }
-    
-    if (isset($_POST['register_btn'])) {
-        $firstname = mysql_real_escape_string($_POST['firstname']);
-        $lastname = mysql_real_escape_string($_POST['lastname']);
-        $email = mysql_real_escape_string($_POST['email']);
-        $password = mysql_real_escape_string($_POST['password']);
-        $password2 = mysql_real_escape_string($_POST['password2']);
+  }
 
-      
+  // Finally, register user if there are no errors in the form
+  if (count($errors) == 0) {
+  	$password = md5($password_1);//encrypt the password before saving in the database
 
-        
-        
-        $db = "SELECT * FROM users WHERE email = '$email'";
-        $result = mysqli_query($con, $db);
-        
-        if ($result == 1) {
-            $_SESSION['message'] = "account already exists";
-            $_SESSION['email'] = $email;
-            header("location: register.php");
-        } else {
-            if ($password == $password2) {
-            //create user
-            $password = md5($password); //hash password before storing for security
-            $db = "INSERT INTO users(firstname, lastname, email, password) VALUES('$firstname','$lastname', '$email', '$password')";
-            mysqli_query($con, $db);
-            
-            $_SESSION['message'] = "you are logged in";
-            $_SESSION['email'] = $email;
-            header("location: home.php");
-            
-            //$_SESSION['message'] = "you are logged in";
-            //$_SESSION['email'] = $email;
-            //header("location: home.php"); //redirect to HP
-        } else {
-            //$_SESSION['message'] = "The two passwords do not match";
-                print "Passwords do not match";
-                
-        }
-    }
-        print_r($_SESSION);
-        }
+  	$query = "INSERT INTO users (firstname, lastname, email, password) 
+  			  VALUES('$firstname', '$lastname', '$email', '$password')";
+  	mysqli_query($db, $query);
+  	$_SESSION['email'] = $email;
+  	$_SESSION['success'] = "You are now logged in";
+  	header('location: index.php');
+  }
+}
 
-        
-        
-   //     if ($password == $password2) {
-            //create user
-            //$password = md5($password); //hash password before //storing for security
-            //$db = "INSERT INTO users(firstname, lastname, email, password) VALUES('$firstname','$lastname', '$email', '$password')";
-            //mysqli_query($con, $db);
-            
-            //$_SESSION['message'] = "you are logged in";
-            //$_SESSION['email'] = $email;
-            //header("location: home.php");
-            
-            //$_SESSION['message'] = "you are logged in";
-            //$_SESSION['email'] = $email;
-            //header("location: home.php"); //redirect to HP
-        //} else {
-     //       $_SESSION['message'] = "The two passwords do not match";
-       // }
-  //  }
+// LOGIN USER
+if (isset($_POST['login_user'])) {
+  $email = mysqli_real_escape_string($db, $_POST['email']);
+  $password = mysqli_real_escape_string($db, $_POST['password']);
+
+  if (empty($email)) {
+  	array_push($errors, "Email is required");
+  }
+  if (empty($password)) {
+  	array_push($errors, "Password is required");
+  }
+
+  if (count($errors) == 0) {
+  	$password = md5($password);
+  	$query = "SELECT * FROM users WHERE email='$email' AND password='$password'";
+  	$results = mysqli_query($db, $query);
+  	if (mysqli_num_rows($results) == 1) {
+  	  $_SESSION['email'] = $email;
+  	  $_SESSION['success'] = "You are now logged in";
+  	  header('location: index.php');
+  	}else {
+  		array_push($errors, "Wrong email/password combination");
+  	}
+  }
+}
 
 ?>
